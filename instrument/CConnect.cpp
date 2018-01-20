2f98a73171b442707943f57fe20351bfd5b484a1
Andrew-Scarlett <1433385675@qq.com>
2018-01-20 10:11:20 +0800

    CConnect

---
+++ instrument/CConnect.cpp
@@ -0,0 +1,4 @@
+#include "CConnect.h"
+int CConnect() {
+
+}
---
+++ instrument/CConnect.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdio.h>
+#include "visa.h"
+
+#pragma comment(lib,"visa32.lib")
+
+__declspec(dllexport) int CConnect();
