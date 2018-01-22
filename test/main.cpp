75ed8224fd9cc6a7173070654e04d3429c00d8e7
Andrew-Scarlett <1433385675@qq.com>
2018-01-22 10:51:08 +0800

    no message

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../instrument/CConnect.h"
+
+#pragma comment(lib,"../instrument/Debug/instrument.lib")
+
+int main() {
+	CConnect();
+	return 0;
+}
